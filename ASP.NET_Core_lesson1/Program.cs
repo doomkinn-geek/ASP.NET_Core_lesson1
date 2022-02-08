33c6eeada77a0fe282637ba1e79337c9dc676f8e
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-02-08 17:29:11 +0300

    init

--- ASP.NET_Core_lesson1/Program.cs
+++ ASP.NET_Core_lesson1/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ASP.NET_Core_lesson1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ ASP.NET_Core_lesson1/ThePost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASP.NET_Core_lesson1
+{
+    public class ThePost
+    {
+        uint userId { get; set; }
+        uint id { get; set; }
+        string title { get; set; }
+        string body { get; set; }
+    }
+}
