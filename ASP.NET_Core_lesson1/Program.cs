486cfd33efcd1d661ddeaa79a20c11ff298405e6
doomkinn@yandex.ru <doomkinn@yandex.ru>
2022-02-08 21:38:07 +0300

    done

--- ASP.NET_Core_lesson1/Program.cs
+++ ASP.NET_Core_lesson1/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
@@ -17,17 +18,35 @@ namespace ASP.NET_Core_lesson1
             ThePost post = null;
             string request = "https://jsonplaceholder.typicode.com/posts/";
 
+            CancellationToken token = new CancellationToken();
+            CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+            token = cancelTokenSource.Token;
             using (var file = new StreamWriter(File.Create("output.txt")))
             {
                 for (int i = 0; i < 11; i++)
                 {                
-                    post = await Request(request + i.ToString(), httpClient);
-                    file.WriteLine(post.ToString());
+                    post = await Request(request + i.ToString(), httpClient, token);
+                    await file.WriteLineAsync(post.ToString());                    
                 }
             }            
+            ConsoleKeyInfo input;
+            while (true)
+            {
+                input = Console.ReadKey();
+                if (input.Key == ConsoleKey.Escape)
+                {
+                    cancelTokenSource.Cancel();
+                    break;
+                }
+            }
+            
         }
-        private static async Task<ThePost> Request(string uri, HttpClient httpClient)
+        private static async Task<ThePost> Request(string uri, HttpClient httpClient, CancellationToken token)
         {
+            if (token.IsCancellationRequested)
+            {
+                return null;
+            }             
             var httpResponse = await httpClient.GetAsync(uri);
             if (httpResponse.Content is object && httpResponse.Content.Headers.ContentType.MediaType == "application/json")
             {
